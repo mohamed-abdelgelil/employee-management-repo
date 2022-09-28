51f62fe6b700867f37368a523cdd151989e44110
Mohamed.Abdelgelil <m.abdelgelil.abdo@gmail.com>
2022-09-29 01:36:11 +0200

    Project Skeleton
    Pushing the project skeleton  with the needed dependencies, libraries and folder structure

---
+++ employee-manage-service/src/main/java/dev/me/employee/EmployeeManageServiceApplication.java
@@ -0,0 +1,13 @@
+package dev.me.employee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeManageServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeManageServiceApplication.class, args);
+	}
+
+}
---
+++ employee-manage-service/src/test/java/dev/me/employee/EmployeeManageServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.me.employee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManageServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
